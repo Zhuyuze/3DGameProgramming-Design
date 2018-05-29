ea7dc9013326ca8cb93602e0abe5d87918978a8b
Zhuyuze <daszhuyuze@foxmail.com>
2018-05-29 23:52:53 +0800

    hw6
    hw6

---
+++ particle/Assets/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float engineRevs;
+	public float exhaustRate;
+
+	ParticleSystem exhaust;
+
+
+	void Start () {
+		exhaust = GetComponent<ParticleSystem>();
+	}
+
+
+	void Update () {
+		exhaust.emissionRate = engineRevs * exhaustRate;
+	}
+
+}
